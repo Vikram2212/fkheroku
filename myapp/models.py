2308a0e1bf0b64514e7c385bd5ee842b12511ea2
Vikram2212 <vikram.c.2212@gmail.com>
2020-04-06 23:13:54 +0530

    Second commit

--- myapp/models.py
+++ myapp/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Search(models.Model):
     search = models.CharField(max_length=200)
     date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.search)
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,44 @@
+import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 from . import models
 
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
+
 def home(request):
+    return render(request,'home.html')
+
+def new_search(request):
     search = request.POST.get('search')
     models.Search.objects.create(search=search)
-    return render(request, 'home.html', {'search': search})
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+
+    final_listings = []
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_img_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_img_url = BASE_IMAGE_URL.format(post_img_id)
+        else:
+            post_img_url = 'No pic'
+
+        final_listings.append((post_title, post_url, post_price, post_img_url))
+
+    frontend= {
+        'search': search,
+        'final_listings': final_listings
+    }
+    return render(request, 'myapp/new_search.html', frontend)
